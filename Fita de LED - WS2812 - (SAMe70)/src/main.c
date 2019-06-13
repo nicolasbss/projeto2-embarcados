c613559ac7794ce6358febe265ec05bbb00f5b93
Gabrielmou <gabrielmoura98@gmail.com>
2019-06-13 19:25:03 -0300

    P2

--- Fita de LED - WS2812 - (SAMe70)/src/main.c
+++ Fita de LED - WS2812 - (SAMe70)/src/main.c
@@ -92,10 +92,6 @@ void but_callback(void)
 	new_order = true;
 }
 
-void but_callback(void)
-{
-	new_order = true;
-}
 
 void new_order_callback(void)
 {
@@ -230,32 +226,6 @@ int main(void)
 				else if(bufferNOME[cont] == '9') numero9(RED);
 				else if(bufferNOME[cont] == '0') numero0(RED);
 			}	
-			//else if(bufferNOME[cont] == 'a' || bufferNOME[cont] == 'A') letraA(BLUE);
-			//else if(bufferNOME[cont] == 'b' || bufferNOME[cont] == 'B') letraB(RED);
-			//else if(bufferNOME[cont] == 'c' || bufferNOME[cont] == 'C') letraC(BLUE);
-			//else if(bufferNOME[cont] == 'd' || bufferNOME[cont] == 'D') letraD(RED);
-			//else if(bufferNOME[cont] == 'e' || bufferNOME[cont] == 'E') letraE(BLUE);
-			//else if(bufferNOME[cont] == 'f' || bufferNOME[cont] == 'F') letraF(BLUE);
-			//else if(bufferNOME[cont] == 'g' || bufferNOME[cont] == 'G') letraG(BLUE);
-			//else if(bufferNOME[cont] == 'h' || bufferNOME[cont] == 'H') letraH(BLUE);
-			//else if(bufferNOME[cont] == 'i' || bufferNOME[cont] == 'I') letraI(GREEN);
-			//else if(bufferNOME[cont] == 'j' || bufferNOME[cont] == 'J') letraJ(BLUE);
-			//else if(bufferNOME[cont] == 'k' || bufferNOME[cont] == 'K') letraK(BLUE);
-			//else if(bufferNOME[cont] == 'l' || bufferNOME[cont] == 'L') letraL(BLUE);
-			//else if(bufferNOME[cont] == 'm' || bufferNOME[cont] == 'M') letraM(BLUE);
-			//else if(bufferNOME[cont] == 'n' || bufferNOME[cont] == 'N') letraN(GREEN);
-			//else if(bufferNOME[cont] == 'o' || bufferNOME[cont] == 'O') letraO(GREEN);
-			//else if(bufferNOME[cont] == 'p' || bufferNOME[cont] == 'P') letraP(BLUE);
-			//else if(bufferNOME[cont] == 'q' || bufferNOME[cont] == 'Q') letraQ(BLUE);
-			//else if(bufferNOME[cont] == 'r' || bufferNOME[cont] == 'R') letraR(BLUE);
-			//else if(bufferNOME[cont] == 's' || bufferNOME[cont] == 'S') letraS(BLUE);
-			//else if(bufferNOME[cont] == 't' || bufferNOME[cont] == 'T') letraT(BLUE);
-			//else if(bufferNOME[cont] == 'u' || bufferNOME[cont] == 'U') letraU(BLUE);
-			//else if(bufferNOME[cont] == 'v' || bufferNOME[cont] == 'V') letraV(GREEN);
-			//else if(bufferNOME[cont] == 'w' || bufferNOME[cont] == 'W') letraW(BLUE);
-			//else if(bufferNOME[cont] == 'x' || bufferNOME[cont] == 'X') letraX(BLUE);
-			//else if(bufferNOME[cont] == 'y' || bufferNOME[cont] == 'Y') letraY(BLUE);
-			//else if(bufferNOME[cont] == 'z' || bufferNOME[cont] == 'Z') letraZ(BLUE);
 			size--;
 			cont++;
 		}
@@ -284,52 +254,3 @@ int main(void)
 
 
 
-
-//TESTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-/*
-while(1){
-	
-	//colorTESTE();
-	//delay_ms(250);
-	data_reset();
-	
-	for(int i = 0; i<1 ; i++){
-		colorRED();
-		colorGREEN();
-		colorBLUE();
-	}
-	
-	for(int i = 0; i<250 ; i++){
-		colorBLACK();
-	}
-	for(int i = 0; i<2; i++){
-		colorWHITE();
-	}
-	
-	
-	data_reset();
-	
-	delay_ms(250);
-	
-	for(int i = 0; i<1 ; i++){
-		colorBLACK();
-		colorBLACK();
-		colorBLACK();
-	}
-	
-	for(int i = 0; i<250 ; i++){
-		colorBLACK();
-	}
-	for(int i = 0; i<2; i++){
-		colorBLACK();
-	}
-	
-	//for(int i = 0; i<290 ; i++){
-	//	colorBLACK();
-	//}
-	
-	data_reset();
-	
-	delay_ms(250);
-	
-}*/
